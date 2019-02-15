363aa2837da551a593cfb62862d531b2de0f8344
Sebastien Devaux <sebastien.devaux@laposte.net>
2019-02-15 21:31:13 +0100

    #0ab48d9 implementing time keeper

--- kern/src/TimeKeeper.cpp
+++ kern/src/TimeKeeper.cpp
@@ -9,6 +9,8 @@
  */
 #include "simph/kern/TimeKeeper.hpp"
 #include <chrono>
+#include <sstream>
+#include <iomanip>
 
 namespace Smp {
 constexpr Smp::Char8 Smp::Services::ITimeKeeper::SMP_TimeKeeper[];
@@ -16,8 +18,17 @@ constexpr Smp::Char8 Smp::Services::ITimeKeeper::SMP_TimeKeeper[];
 
 namespace simph {
 	namespace kern {
-
-Smp::DateTime TimeKeeper::_y2kJan1Offset=100000000;
+// ..........................................................
+Smp::DateTime __GetY2KOffset() {
+    std::tm tm = {};
+    std::stringstream ss("2000-01-01 00:00:00+0000");
+    ss >> std::get_time(&tm, "%Y-%m-%d %H:%M:%S%z");
+    auto tp = std::chrono::system_clock::from_time_t(std::mktime(&tm));
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(
+                tp.time_since_epoch()).count();
+    
+}
+Smp::DateTime TimeKeeper::_y2kJan1Offset=__GetY2KOffset();
 // --------------------------------------------------------------------
 // ..........................................................
 TimeKeeper::TimeKeeper(Smp::String8 name,
@@ -43,7 +54,7 @@ Smp::DateTime TimeKeeper::GetMissionStartTime() const {
 }
 // ..........................................................
 Smp::DateTime TimeKeeper::GetMissionTime() const {
-    return _epochOffset + _simTime - _missionStart;
+    return GetEpochTime() - _missionStart;
 }
 // ..........................................................
 Smp::DateTime TimeKeeper::GetZuluTime() const {
@@ -52,5 +63,43 @@ Smp::DateTime TimeKeeper::GetZuluTime() const {
             - _y2kJan1Offset;
     
 }
+// --------------------------------------------------------------------
+// ..........................................................
+void TimeKeeper::SetSimulationTime(Smp::Duration simulationTime) {
+    if (simulationTime>_simTime) {
+        _simTime=simulationTime;
+    }
+    else {
+// TODO throw exception TBD.
+    }
+}
+// ..........................................................
+void TimeKeeper::SetEpochTime(Smp::DateTime epochTime) {
+    _epochOffset=epochTime-_simTime;
+// TODO fire event TBD.
+}
+// ..........................................................
+void TimeKeeper::SetMissionStartTime(Smp::DateTime missionStart) {
+    _missionStart=missionStart;
+// TODO fire event TBD.
+}
+// ..........................................................
+void TimeKeeper::SetMissionTime(Smp::Duration missionTime) {
+    _missionStart=GetEpochTime() - missionTime;
+// TODO fire event TBD.
+}
+// --------------------------------------------------------------------
+// ..........................................................
+void TimeKeeper::reset() {
+    // default : set epoch time to current zulu time.
+    // set simTime to 0
+    // set missionTime to 0 too.
+    _simTime=0;
+    _epochOffset=GetZuluTime();
+    _missionStart=_epochOffset;
+}
+
+
+
 
 }} // namespace simph::kern
