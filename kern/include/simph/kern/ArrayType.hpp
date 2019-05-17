56148f360daf1c13dc56c809586132eae2d1df25
Sebastien Devaux <sdevaux@laposte.net>
2019-05-17 21:48:37 +0200

    #0ab48d9 implementing TypeManager

---
+++ kern/include/simph/kern/ArrayType.hpp
@@ -0,0 +1,43 @@
+/*
+ * @file ArrayType.h
+ *
+ * Copyright 2019 . All rights reserved.
+ * Use is subject to license terms.
+ *
+ * $Id$
+ * $Date$
+ */
+#ifndef __simph_kern_ArrayType_HPP__
+#define __simph_kern_ArrayType_HPP__
+#include "simph/kern/Type.hpp"
+#include "Smp/Publication/IArrayType.h"
+
+namespace simph {
+	namespace kern {
+
+/**
+ *
+ */
+class ArrayType: public Type,
+        virtual public Smp::Publication::IArrayType {
+public:
+    /**
+     * Default constructor.
+     */
+    ArrayType(Smp::Uuid uuid, Smp::String8 name, Smp::String8 descr, 
+            Smp::IObject* parent,
+            Smp::UInt64 size,Smp::Publication::IType* type);
+    /**
+     * Destructor.
+     */
+    virtual ~ArrayType();
+    // Smp::Publication::IArrayType implementation
+    Smp::UInt64 GetSize() const override;
+    const Smp::Publication::IType* GetItemType() const override;
+private:
+    Smp::UInt64 _size;
+    const Smp::Publication::IType* _itemType;
+};
+
+}} // namespace simph::kern
+#endif // __simph_kern_ArrayType_HPP__
---
+++ kern/src/ArrayType.cpp
@@ -0,0 +1,33 @@
+/*
+ * @file ArrayType.cpp
+ *
+ * Copyright 2019 . All rights reserved.
+ * Use is subject to license terms.
+ *
+ * $Id$
+ * $Date$
+ */
+#include "simph/kern/ArrayType.hpp"
+
+namespace simph {
+	namespace kern {
+// --------------------------------------------------------------------
+// ..........................................................
+ArrayType::ArrayType(Smp::Uuid uuid, Smp::String8 name, Smp::String8 descr, 
+        Smp::IObject* parent, Smp::UInt64 size, Smp::Publication::IType* type):
+        Type(uuid,type->GetPrimitiveTypeKind(),name,descr,parent), 
+        _size(size), _itemType(type) {
+}
+// ..........................................................
+ArrayType::~ArrayType() {
+}
+// --------------------------------------------------------------------
+// ..........................................................
+Smp::UInt64 ArrayType::GetSize() const {
+    return _size;
+}
+// ..........................................................
+const Smp::Publication::IType* ArrayType::GetItemType() const {
+    return _itemType;
+}
+}} // namespace simph::kern
