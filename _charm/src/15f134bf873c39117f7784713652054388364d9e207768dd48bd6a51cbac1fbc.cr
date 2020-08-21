99b6be7d815a776517ac6e19d53bd8b759f6693c
Sebastien Devaux <sebastien.devaux@eduvax.net>
2020-08-21 16:43:34 +0200

    issue management

---
+++ _charm/src/15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc.cr
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc" state="open">
+<title>Release simph 0.1</title>
+<reporter>sdevaux</reporter>
+<creation>2020-08-21 16:43:34+02:00</creation>
+<description></description>
+<links>
+<link name="parent"></link>
+<link name="child">6dbca695c7cb8f36ef6e21b2ab5f632de9f93fee9bdd37340ebb71947e00b026</link>
+</links>
+<cf v="null"/>
+</cr>
---
+++ _charm/src/6dbca695c7cb8f36ef6e21b2ab5f632de9f93fee9bdd37340ebb71947e00b026.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="6dbca695c7cb8f36ef6e21b2ab5f632de9f93fee9bdd37340ebb71947e00b026" state="open">
+<title>plop</title>
+<reporter>sdevaux</reporter>
+<creation>2020-08-21 16:43:34+02:00</creation>
+<description></description>
+<links>
+<link name="parent">15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc</link>
+</links>
+<cf v="null"/>
+</cr>
