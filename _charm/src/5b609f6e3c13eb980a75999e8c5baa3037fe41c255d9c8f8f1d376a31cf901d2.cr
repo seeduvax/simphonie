d2a68dff433c1e73a612a7f66e72ae4254217d9f
Sebastien Devaux <sebastien.devaux@eduvax.net>
2020-12-03 14:16:47 +0100

    #5b609f6 trying to set the logo in readme...

--- _charm/src/5b609f6e3c13eb980a75999e8c5baa3037fe41c255d9c8f8f1d376a31cf901d2.cr
+++ _charm/src/5b609f6e3c13eb980a75999e8c5baa3037fe41c255d9c8f8f1d376a31cf901d2.cr
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc</link>
 </links>
-<cf v="sdevaux 2020-12-03T14:15:45+01:00"/>
+<cf v="sdevaux 2020-12-03T14:16:59+01:00"/>
 </cr>
