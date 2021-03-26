b6664c80380c1c54461bd29f82f1af731b435a7c
Mael COURONNE <mael.couronne@ariane.group>
2021-03-26 14:52:56 +0100

    #15f134b issue management

--- _charm/src/a15530e29df5d17a4358f2def3de20b45fdc27aeeaf9ccd6f0c176487af8b114.cr
+++ _charm/src/a15530e29df5d17a4358f2def3de20b45fdc27aeeaf9ccd6f0c176487af8b114.cr
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="a15530e29df5d17a4358f2def3de20b45fdc27aeeaf9ccd6f0c176487af8b114" state="working">
-<title>Implement fields propagation/push</title>
+<cr id="a15530e29df5d17a4358f2def3de20b45fdc27aeeaf9ccd6f0c176487af8b114" state="open">
+<title></title>
 <reporter>f001864</reporter>
-<creation>2021-03-25 15:52:33+01:00</creation>
+<creation>2021-03-26 14:52:56+01:00</creation>
 <description></description>
 <links>
 <link name="parent">15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc</link>
 </links>
-<cf v="f001864 2021-03-26T14:50:17+01:00"/>
+<cf v="null"/>
 </cr>
