07dd988c1743a376bfe3c018918bf68a8588962b
Mael COURONNE <mael.couronne@ariane.group>
2021-04-16 09:47:59 +0200

    #15f134b issue management

--- _charm/src/15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc.cr
+++ _charm/src/15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc.cr
@@ -18,6 +18,7 @@
 <link name="child">ce4f97a2d4176a45e4d45ee5d27de1b79298c3ffe8004804043716e011cc914a</link>
 <link name="child">c4a739ee1529a13e7fc28a0f5218a4e6363ba482039daf51dba83b75af036506</link>
 <link name="child">9d73a160017d590db375ca5a7396e7cd85af166ae33b35d239bd4a57fc699824</link>
+<link name="child">f205c9f8943b68134f0a4680e4ad8b7a1eb9f6af3b6ec55cfc3740b63ba52434</link>
 </links>
 <cf v="sdevaux 2020-08-21T17:06:25+02:00"/>
 </cr>
---
+++ _charm/src/f205c9f8943b68134f0a4680e4ad8b7a1eb9f6af3b6ec55cfc3740b63ba52434.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="f205c9f8943b68134f0a4680e4ad8b7a1eb9f6af3b6ec55cfc3740b63ba52434" state="open">
+<title>Add documentation about SMPC implementation progress report and status</title>
+<reporter>f001864</reporter>
+<creation>2021-04-16 09:47:59+02:00</creation>
+<description></description>
+<links>
+<link name="parent">15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc</link>
+</links>
+<cf v="null"/>
+</cr>
