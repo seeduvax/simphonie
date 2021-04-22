db07afb167ffdac559986ae78cb54a52830261bd
Mael COURONNE <mael.couronne@ariane.group>
2021-04-22 14:31:02 +0200

    #15f134b issue management

---
+++ _charm/src/91abc1e69b7aed145963ac184f5e96f9915d859d629cf07cc221f6d035a9d874.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="91abc1e69b7aed145963ac184f5e96f9915d859d629cf07cc221f6d035a9d874" state="open">
+<title>Add first IFailure implementation</title>
+<reporter>f001864</reporter>
+<creation>2021-04-22 14:31:02+02:00</creation>
+<description></description>
+<links>
+<link name="parent">15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc</link>
+</links>
+<cf v="null"/>
+</cr>
