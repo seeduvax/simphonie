e6b24b64d72bc965b8fa73faf4d87c38f97eccd2
Mael COURONNE <mael.couronne@ariane.group>
2021-05-04 16:51:58 +0200

    #15f134b issue management

--- _charm/src/15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc.cr
+++ _charm/src/15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc.cr
@@ -32,6 +32,7 @@
 <link name="child">31b67ea64815ecbc6cecbe4ce824068a1b85083a3e24acb73141ec063c30c598</link>
 <link name="child">5f1bf2d4116c6121a33d3d889b7b9b103e818e83138f82e4e6bc061d80c6601f</link>
 <link name="child">b370f5b7b795e17e23a0042a9ca4d33a24e968cb5ec8e0475016184fc2fc886e</link>
+<link name="child">c48bce2848de0ad34b8dcd7c9fba7c0d531b80c25f2c6da623811b349ea5bdc6</link>
 </links>
 <cf v="sdevaux 2020-08-21T17:06:25+02:00"/>
 </cr>
---
+++ _charm/src/c48bce2848de0ad34b8dcd7c9fba7c0d531b80c25f2c6da623811b349ea5bdc6.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="c48bce2848de0ad34b8dcd7c9fba7c0d531b80c25f2c6da623811b349ea5bdc6" state="open">
+<title>Minors fix</title>
+<reporter>f001864</reporter>
+<creation>2021-05-04 16:51:58+02:00</creation>
+<description></description>
+<links>
+<link name="parent">15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc</link>
+</links>
+<cf v="null"/>
+</cr>
