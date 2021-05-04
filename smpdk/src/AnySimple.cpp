459dea7fa072f1f0635ed48f6ab69a6b62d452df
Mael COURONNE <mael.couronne@ariane.group>
2021-05-04 15:35:10 +0200

    #31b67ea fix AnySimple

--- smpdk/src/AnySimple.cpp
+++ smpdk/src/AnySimple.cpp
@@ -1,6 +1,7 @@
 #include "Smp/AnySimple.h"
 #include <string.h>
 #include <iostream>
+#include <sstream>
 
 namespace Smp {
 
@@ -24,6 +25,65 @@ AnySimple& AnySimple::operator=(AnySimple&& other) {
     type = std::move(other.type);
     value = std::move(other.value);
     return *this;
+}
+// ..........................................................
+std::ostream& operator<<(std::ostream& os, const Smp::AnySimple& obj) {
+    // Associate corresponding primitive type
+    switch (obj.GetType()) {
+        case Smp::PrimitiveTypeKind::PTK_Char8:
+            os << obj.value.char8Value;
+            break;
+        case Smp::PrimitiveTypeKind::PTK_Bool:
+            os << obj.value.boolValue;
+            break;
+        case Smp::PrimitiveTypeKind::PTK_Int8:
+            os << obj.value.int8Value;
+            break;
+        case Smp::PrimitiveTypeKind::PTK_UInt8:
+            os << obj.value.uInt8Value;
+            break;
+        case Smp::PrimitiveTypeKind::PTK_Int16:
+            os << obj.value.int16Value;
+            break;
+        case Smp::PrimitiveTypeKind::PTK_UInt16:
+            os << obj.value.uInt16Value;
+            break;
+        case Smp::PrimitiveTypeKind::PTK_Int32:
+            os << obj.value.int32Value;
+            break;
+        case Smp::PrimitiveTypeKind::PTK_UInt32:
+            os << obj.value.uInt32Value;
+            break;
+        case Smp::PrimitiveTypeKind::PTK_Int64:
+            os << obj.value.int64Value;
+            break;
+        case Smp::PrimitiveTypeKind::PTK_UInt64:
+            os << obj.value.uInt64Value;
+            break;
+        case Smp::PrimitiveTypeKind::PTK_Float32:
+            os << obj.value.float32Value;
+            break;
+        case Smp::PrimitiveTypeKind::PTK_Float64:
+            os << obj.value.float64Value;
+            break;
+        case Smp::PrimitiveTypeKind::PTK_Duration:
+            os << obj.value.durationValue;
+            break;
+        case Smp::PrimitiveTypeKind::PTK_DateTime:
+            os << obj.value.dateTimeValue;
+            break;
+        case Smp::PrimitiveTypeKind::PTK_String8:
+            os << obj.value.string8Value;
+            break;
+        default:
+            std::stringstream ss;
+            ss << "Primitive type " << obj.GetType() << " not found";
+            // TODO logical_error
+            throw std::runtime_error(ss.str().c_str());
+            break;
+    }
+
+    return os;
 }
 // ..........................................................
 AnySimple::~AnySimple() noexcept {}
