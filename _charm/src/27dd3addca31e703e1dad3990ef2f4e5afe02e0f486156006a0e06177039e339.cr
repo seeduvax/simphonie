3bf71ad97d082fc3e460b51eb01b300f2e5f61ce
Mael COURONNE <mael.couronne@ariane.group>
2021-05-04 15:32:08 +0200

    #15f134b issue management

---
+++ _charm/src/27dd3addca31e703e1dad3990ef2f4e5afe02e0f486156006a0e06177039e339.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="27dd3addca31e703e1dad3990ef2f4e5afe02e0f486156006a0e06177039e339" state="open">
+<title>update Failure</title>
+<reporter>f001864</reporter>
+<creation>2021-05-04 15:32:08+02:00</creation>
+<description></description>
+<links>
+<link name="parent">15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc</link>
+</links>
+<cf v="null"/>
+</cr>
