6cf069459e92b3b3c8af4e2473815456bf233bb5
Mael COURONNE <mael.couronne@ariane.group>
2021-05-11 16:53:42 +0200

    #15f134b issue management

---
+++ _charm/src/f9b287c51f0c61f7b50690bf0a4d295ad6f96dee0e513ed48fcaa8c451fb861e.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="f9b287c51f0c61f7b50690bf0a4d295ad6f96dee0e513ed48fcaa8c451fb861e" state="open">
+<title>recursive resolve function, stable version support . / [] ...</title>
+<reporter>f001864</reporter>
+<creation>2021-05-11 16:53:42+02:00</creation>
+<description></description>
+<links>
+<link name="parent">15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc</link>
+</links>
+<cf v="null"/>
+</cr>
