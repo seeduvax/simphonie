b993a53ce656cfc5f723909e5da47183b4cc1bcc
Mael COURONNE <mael.couronne@ariane.group>
2021-05-11 17:02:24 +0200

    #15f134b issue management

---
+++ _charm/src/189f078ca4b608ad1f79898de504f2a776fdb905d952bb0b8132383008b7fd2c.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="189f078ca4b608ad1f79898de504f2a776fdb905d952bb0b8132383008b7fd2c" state="open">
+<title>Postpone addChild(EntryPoint) to publish function in Simulator (needed for external simphonie models)</title>
+<reporter>f001864</reporter>
+<creation>2021-05-11 17:02:24+02:00</creation>
+<description></description>
+<links>
+<link name="parent">15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc</link>
+</links>
+<cf v="null"/>
+</cr>
