3ebf3f2c8a57aa3404683b9a0436233d930b2e2e
Andre ASTYL <andre.astyl@ariane.group>
2021-05-13 19:49:57 +0200

    #1498442 close issue management

--- _charm/src/1498442fd55d92d026aeba3d68803962a97e545f929ee47c15494064c7809a74.cr
+++ _charm/src/1498442fd55d92d026aeba3d68803962a97e545f929ee47c15494064c7809a74.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="1498442fd55d92d026aeba3d68803962a97e545f929ee47c15494064c7809a74" state="open">
+<cr id="1498442fd55d92d026aeba3d68803962a97e545f929ee47c15494064c7809a74" state="closed" delivered="simph-0.1.2">
 <title>Update Sampler: minor update, file is created at init entry point, some helper improvment (callback and simu helper to find object)</title>
 <reporter>aa</reporter>
 <creation>2021-05-13 19:46:11+02:00</creation>
