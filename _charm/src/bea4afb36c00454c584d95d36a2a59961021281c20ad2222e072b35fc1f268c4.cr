9b745f77088a44b0918b8af148d18f904961c184
Mael COURONNE <mael.couronne@ariane.group>
2021-05-28 12:14:26 +0200

    #15f134b issue management

---
+++ _charm/src/bea4afb36c00454c584d95d36a2a59961021281c20ad2222e072b35fc1f268c4.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="bea4afb36c00454c584d95d36a2a59961021281c20ad2222e072b35fc1f268c4" state="open">
+<title>update documentation: todo tasks</title>
+<reporter>f001864</reporter>
+<creation>2021-05-28 12:14:26+02:00</creation>
+<description></description>
+<links>
+<link name="parent">15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc</link>
+</links>
+<cf v="null"/>
+</cr>
