31da5c7b28f5600bfdc4c66c9b631ad780641d73
Mael COURONNE <mael.couronne@ariane.group>
2021-06-04 16:11:57 +0200

    #15f134b issue management

--- _charm/src/15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc.cr
+++ _charm/src/15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc.cr
@@ -43,6 +43,7 @@
 <link name="child">9d73a160017d590db375ca5a7396e7cd85af166ae33b35d239bd4a57fc699824</link>
 <link name="child">bea4afb36c00454c584d95d36a2a59961021281c20ad2222e072b35fc1f268c4</link>
 <link name="child">393a660ed969cfe07eab1f99619622b0d382d52c31aa0b17331441737e9c8f1a</link>
+<link name="child">5e4691f25b96111afc7aed68b2bd7694e6a6e74ab9d538d7bff6a84425ce1523</link>
 </links>
 <cf v="sdevaux 2020-08-21T17:06:25+02:00"/>
 </cr>
---
+++ _charm/src/5e4691f25b96111afc7aed68b2bd7694e6a6e74ab9d538d7bff6a84425ce1523.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="5e4691f25b96111afc7aed68b2bd7694e6a6e74ab9d538d7bff6a84425ce1523" state="open">
+<title>high lvl lua sampler support (not work)</title>
+<reporter>f001864</reporter>
+<creation>2021-06-04 16:11:54+02:00</creation>
+<description></description>
+<links>
+<link name="parent">15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc</link>
+</links>
+<cf v="null"/>
+</cr>
