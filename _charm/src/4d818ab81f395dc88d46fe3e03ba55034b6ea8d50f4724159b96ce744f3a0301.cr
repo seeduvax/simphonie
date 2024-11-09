b245a26baee390f5b90a6c3760051238630bcb9d
Sebastien Devaux <sebastien.devaux@eduvax.net>
2024-11-09 18:56:13 +0100

    #4d818ab no more link with libcrypt

--- _charm/src/4d818ab81f395dc88d46fe3e03ba55034b6ea8d50f4724159b96ce744f3a0301.cr
+++ _charm/src/4d818ab81f395dc88d46fe3e03ba55034b6ea8d50f4724159b96ce744f3a0301.cr
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">15f134bf873c39117f7784713652054388364d9e207768dd48bd6a51cbac1fbc</link>
 </links>
-<cf v="sdevaux 2024-11-09T18:52:51+01:00"/>
+<cf v="sebas 2024-11-09T18:56:45+01:00"/>
 </cr>
