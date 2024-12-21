1964986c255784d829c837a9cfaa6ca7a5a7638f
Sebastien Devaux <sebastien.devaux@eduvax.net>
2024-12-21 16:33:45 +0100

    #e56ba90 issue management

--- _charm/src/e56ba90338b15805e882937880d547edc3eb47a740ce7b600002b1010d87dfd3.cr
+++ _charm/src/e56ba90338b15805e882937880d547edc3eb47a740ce7b600002b1010d87dfd3.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="e56ba90338b15805e882937880d547edc3eb47a740ce7b600002b1010d87dfd3" state="open">
+<cr id="e56ba90338b15805e882937880d547edc3eb47a740ce7b600002b1010d87dfd3" state="working">
 <title>Release simph 1.0</title>
 <reporter>sdevaux</reporter>
 <creation>2024-12-21 16:29:49+01:00</creation>
@@ -8,5 +8,5 @@
 <link name="parent"></link>
 <link name="child">1e2989eaa730c854f52b0da90aacba212cf83bbea680e7266185fddc1f7d118f</link>
 </links>
-<cf v="null"/>
+<cf v="sdevaux 2024-12-21T16:33:58+01:00"/>
 </cr>
