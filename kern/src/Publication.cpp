17c2ad3ef402fb0ead3aba37d23e01583fc1b800
Sebastien Devaux <sebastien.devaux@eduvax.net>
2024-12-27 18:11:41 +0100

    #5101d4f wip fields publication

--- kern/src/Publication.cpp
+++ kern/src/Publication.cpp
@@ -40,12 +40,6 @@ Publication::Publication(Smp::IObject* toPublish, Smp::Publication::ITypeRegistr
       _typeRegistry(typeRegistry),
       _fields("fields", "", this),
       _properties("properties", "", this) {
-    // Smp::IEntryPointPublisher* epp = dynamic_cast<Smp::IEntryPointPublisher*>(toPublish);
-    // if (epp != nullptr) {
-    //    for (auto ep : *(epp->GetEntryPoints())) {
-    //        addChild(ep);
-    //    }
-    //}
 }
 // ..........................................................
 Publication::~Publication() {
@@ -112,77 +106,77 @@ Smp::IField* Publication::PublishField(Smp::String8 name, Smp::String8 descripti
 // ..........................................................
 Smp::IField* Publication::PublishField(Smp::String8 name, Smp::String8 description, Smp::Bool* address, Smp::ViewKind view,
                                Smp::Bool state, Smp::Bool input, Smp::Bool output) {
-    auto f=new TField<Smp::Bool>(name, description, view, address, state, input, output, getPubObj());
+    auto f=Field::Create<Smp::Bool>(name, description, view, address, state, input, output, getPubObj());
     addField(f);
     return f;
 }
 // ..........................................................
 Smp::IField* Publication::PublishField(Smp::String8 name, Smp::String8 description, Smp::Int8* address, Smp::ViewKind view,
                                Smp::Bool state, Smp::Bool input, Smp::Bool output) {
-    auto f=new TField<Smp::Int8>(name, description, view, address, state, input, output, getPubObj());
+    auto f=Field::Create<Smp::Int8>(name, description, view, address, state, input, output, getPubObj());
     addField(f);
     return f;
 }
 // ..........................................................
 Smp::IField* Publication::PublishField(Smp::String8 name, Smp::String8 description, Smp::Int16* address, Smp::ViewKind view,
                                Smp::Bool state, Smp::Bool input, Smp::Bool output) {
-    auto f=new TField<Smp::Int16>(name, description, view, address, state, input, output, getPubObj());
+    auto f=Field::Create<Smp::Int16>(name, description, view, address, state, input, output, getPubObj());
     addField(f);
     return f;
 }
 // ..........................................................
 Smp::IField* Publication::PublishField(Smp::String8 name, Smp::String8 description, Smp::Int32* address, Smp::ViewKind view,
                                Smp::Bool state, Smp::Bool input, Smp::Bool output) {
-    auto f=new TField<Smp::Int32>(name, description, view, address, state, input, output, getPubObj());
+    auto f=Field::Create<Smp::Int32>(name, description, view, address, state, input, output, getPubObj());
     addField(f);
     return f;
 }
 // ..........................................................
 Smp::IField* Publication::PublishField(Smp::String8 name, Smp::String8 description, Smp::Int64* address, Smp::ViewKind view,
                                Smp::Bool state, Smp::Bool input, Smp::Bool output) {
-    auto f=new TField<Smp::Int64>(name, description, view, address, state, input, output, getPubObj());
+    auto f=Field::Create<Smp::Int64>(name, description, view, address, state, input, output, getPubObj());
     addField(f);
     return f;
 }
 // ..........................................................
 Smp::IField* Publication::PublishField(Smp::String8 name, Smp::String8 description, Smp::UInt8* address, Smp::ViewKind view,
                                Smp::Bool state, Smp::Bool input, Smp::Bool output) {
-    auto f=new TField<Smp::UInt8>(name, description, view, address, state, input, output, getPubObj());
+    auto f=Field::Create<Smp::UInt8>(name, description, view, address, state, input, output, getPubObj());
     addField(f);
     return f;
 }
 // ..........................................................
 Smp::IField* Publication::PublishField(Smp::String8 name, Smp::String8 description, Smp::UInt16* address, Smp::ViewKind view,
                                Smp::Bool state, Smp::Bool input, Smp::Bool output) {
-    auto f=new TField<Smp::UInt16>(name, description, view, address, state, input, output, getPubObj());
+    auto f=Field::Create<Smp::UInt16>(name, description, view, address, state, input, output, getPubObj());
     addField(f);
     return f;
 }
 // ..........................................................
 Smp::IField* Publication::PublishField(Smp::String8 name, Smp::String8 description, Smp::UInt32* address, Smp::ViewKind view,
                                Smp::Bool state, Smp::Bool input, Smp::Bool output) {
-    auto f=new TField<Smp::UInt32>(name, description, view, address, state, input, output, getPubObj());
+    auto f=Field::Create<Smp::UInt32>(name, description, view, address, state, input, output, getPubObj());
     addField(f);
     return f;
 }
 // ..........................................................
 Smp::IField* Publication::PublishField(Smp::String8 name, Smp::String8 description, Smp::UInt64* address, Smp::ViewKind view,
                                Smp::Bool state, Smp::Bool input, Smp::Bool output) {
-    auto f=new TField<Smp::UInt64>(name, description, view, address, state, input, output, getPubObj());
+    auto f=Field::Create<Smp::UInt64>(name, description, view, address, state, input, output, getPubObj());
     addField(f);
     return f;
 }
 // ..........................................................
 Smp::IField* Publication::PublishField(Smp::String8 name, Smp::String8 description, Smp::Float32* address, Smp::ViewKind view,
                                Smp::Bool state, Smp::Bool input, Smp::Bool output) {
-    auto f=new TField<Smp::Float32>(name, description, view, address, state, input, output, getPubObj());
+    auto f=Field::Create<Smp::Float32>(name, description, view, address, state, input, output, getPubObj());
     addField(f);
     return f;
 }
 // ..........................................................
 Smp::IField* Publication::PublishField(Smp::String8 name, Smp::String8 description, Smp::Float64* address, Smp::ViewKind view,
                                Smp::Bool state, Smp::Bool input, Smp::Bool output) {
-    auto f=new TField<Smp::Float64>(name, description, view, address, state, input, output, getPubObj());
+    auto f=Field::Create<Smp::Float64>(name, description, view, address, state, input, output, getPubObj());
     addField(f);
     return f;
 }
