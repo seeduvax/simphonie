5f847616a8be1db0af1654ceb5bd8fe28f6e5341
Sebastien Devaux <sebastien.devaux@eduvax.net>
2024-12-27 18:52:59 +0100

    #871f47c doc scheduler / time keeper coordination

--- _charm/src/871f47cde3162ca135d4d1761de548ed199d0df5cef7b324423766ad463e256e.cr
+++ _charm/src/871f47cde3162ca135d4d1761de548ed199d0df5cef7b324423766ad463e256e.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="871f47cde3162ca135d4d1761de548ed199d0df5cef7b324423766ad463e256e" state="open">
+<cr id="871f47cde3162ca135d4d1761de548ed199d0df5cef7b324423766ad463e256e" state="working">
 <title>Simulation start/stop management</title>
 <reporter>sdevaux</reporter>
 <creation>2024-12-27 19:09:20+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">e56ba90338b15805e882937880d547edc3eb47a740ce7b600002b1010d87dfd3</link>
 </links>
-<cf v="null"/>
+<cf v="sdevaux 2024-12-27T18:53:14+01:00"/>
 </cr>
