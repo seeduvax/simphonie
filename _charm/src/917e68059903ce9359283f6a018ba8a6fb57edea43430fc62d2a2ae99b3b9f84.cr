a8c72df9c36a4af2a39d761c71ca2fbd20a2d263
Sebastien Devaux <sebastien.devaux@eduvax.net>
2025-01-03 12:44:53 +0100

    #e56ba90 issue management

---
+++ _charm/src/917e68059903ce9359283f6a018ba8a6fb57edea43430fc62d2a2ae99b3b9f84.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="917e68059903ce9359283f6a018ba8a6fb57edea43430fc62d2a2ae99b3b9f84" state="open">
+<title>Publication and resolver using Add/GetChild</title>
+<reporter>sdevaux</reporter>
+<creation>2025-01-03 12:44:53+01:00</creation>
+<description></description>
+<links>
+<link name="parent">e56ba90338b15805e882937880d547edc3eb47a740ce7b600002b1010d87dfd3</link>
+</links>
+<cf v="null"/>
+</cr>
