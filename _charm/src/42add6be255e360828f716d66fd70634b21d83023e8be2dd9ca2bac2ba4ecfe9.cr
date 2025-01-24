6cbdc5cc16bc1ac0128c772c945ea286ba0a4c4a
Sebastien Devaux <sebastien.devaux@eduvax.net>
2025-01-24 14:14:46 +0100

    #e56ba90 issue management

---
+++ _charm/src/42add6be255e360828f716d66fd70634b21d83023e8be2dd9ca2bac2ba4ecfe9.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="42add6be255e360828f716d66fd70634b21d83023e8be2dd9ca2bac2ba4ecfe9" state="open">
+<title>Refactor Sampler as true data recording service</title>
+<reporter>sdevaux</reporter>
+<creation>2025-01-24 14:14:46+01:00</creation>
+<description></description>
+<links>
+<link name="parent">e56ba90338b15805e882937880d547edc3eb47a740ce7b600002b1010d87dfd3</link>
+</links>
+<cf v="null"/>
+</cr>
--- _charm/src/e56ba90338b15805e882937880d547edc3eb47a740ce7b600002b1010d87dfd3.cr
+++ _charm/src/e56ba90338b15805e882937880d547edc3eb47a740ce7b600002b1010d87dfd3.cr
@@ -12,6 +12,7 @@
 <link name="child">917e68059903ce9359283f6a018ba8a6fb57edea43430fc62d2a2ae99b3b9f84</link>
 <link name="child">59e9ed5497dc67d7905bddaa2390391ef714b24852a7d354d95876ed7f3129cc</link>
 <link name="child">8d66b4599978ed41bc0dfad3bf2f490121c30bcd72cef495e37468a8493a74ef</link>
+<link name="child">42add6be255e360828f716d66fd70634b21d83023e8be2dd9ca2bac2ba4ecfe9</link>
 </links>
 <cf v="sdevaux 2024-12-21T16:33:58+01:00"/>
 </cr>
