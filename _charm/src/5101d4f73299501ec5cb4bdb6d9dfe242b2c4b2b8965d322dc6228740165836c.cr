39afe728e52e8a145427911ac5f83e43a8b80864
Sebastien Devaux <sebastien.devaux@eduvax.net>
2025-02-05 09:08:56 +0100

    #5101d4f Added invalid field name exception

--- _charm/src/5101d4f73299501ec5cb4bdb6d9dfe242b2c4b2b8965d322dc6228740165836c.cr
+++ _charm/src/5101d4f73299501ec5cb4bdb6d9dfe242b2c4b2b8965d322dc6228740165836c.cr
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">e56ba90338b15805e882937880d547edc3eb47a740ce7b600002b1010d87dfd3</link>
 </links>
-<cf v="sdevaux 2025-02-04T22:15:20+01:00"/>
+<cf v="sdevaux 2025-02-05T09:09:12+01:00"/>
 </cr>
