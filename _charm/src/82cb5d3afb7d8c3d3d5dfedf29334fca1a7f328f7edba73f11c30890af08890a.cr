523e099dfbcadd8c5ec0bc40eb1b5ea3aab7a0c5
Sebastien Devaux <sebastien.devaux@eduvax.net>
2025-02-18 22:41:25 +0100

    #82cb5d3 issue description

--- _charm/src/82cb5d3afb7d8c3d3d5dfedf29334fca1a7f328f7edba73f11c30890af08890a.cr
+++ _charm/src/82cb5d3afb7d8c3d3d5dfedf29334fca1a7f328f7edba73f11c30890af08890a.cr
@@ -1,11 +1,14 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="82cb5d3afb7d8c3d3d5dfedf29334fca1a7f328f7edba73f11c30890af08890a" state="open">
+<cr id="82cb5d3afb7d8c3d3d5dfedf29334fca1a7f328f7edba73f11c30890af08890a" state="working">
 <title>namespace / branch name / application name discrepency</title>
 <reporter>sdevaux</reporter>
 <creation>2025-02-18 22:18:04+01:00</creation>
-<description></description>
+<description>
+<p>Change simph to simphonie as application name and 1st level naemspace</p>
+<p>Caution: will need to change branch tracking issue id (then all links for all branch's child issues.</p>
+</description>
 <links>
 <link name="parent">e56ba90338b15805e882937880d547edc3eb47a740ce7b600002b1010d87dfd3</link>
 </links>
-<cf v="null"/>
+<cf v="sdevaux 2025-02-18T22:41:33+01:00"/>
 </cr>
