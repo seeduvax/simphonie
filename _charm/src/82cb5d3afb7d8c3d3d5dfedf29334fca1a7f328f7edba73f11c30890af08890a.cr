e5c1048a1478afa15966be42e548a31c73d9c659
Sebastien Devaux <sebastien.devaux@eduvax.net>
2025-02-18 22:18:04 +0100

    #e56ba90 issue management

---
+++ _charm/src/82cb5d3afb7d8c3d3d5dfedf29334fca1a7f328f7edba73f11c30890af08890a.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="82cb5d3afb7d8c3d3d5dfedf29334fca1a7f328f7edba73f11c30890af08890a" state="open">
+<title>namespace / branch name / application name discrepency</title>
+<reporter>sdevaux</reporter>
+<creation>2025-02-18 22:18:04+01:00</creation>
+<description></description>
+<links>
+<link name="parent">e56ba90338b15805e882937880d547edc3eb47a740ce7b600002b1010d87dfd3</link>
+</links>
+<cf v="null"/>
+</cr>
