98690187073c38a44d539ad28db734504da84071
Sebastien Devaux <sebastien.devaux@eduvax.net>
2025-02-18 22:21:29 +0100

    #e56ba90 issue management

---
+++ _charm/src/c19610976effdbebf71c202fb721af73ae179d30d89b934a540bf212af98cb36.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="c19610976effdbebf71c202fb721af73ae179d30d89b934a540bf212af98cb36" state="open">
+<title>Move smpd as a standalone component managed with its own branches</title>
+<reporter>sdevaux</reporter>
+<creation>2025-02-18 22:21:29+01:00</creation>
+<description></description>
+<links>
+<link name="parent">e56ba90338b15805e882937880d547edc3eb47a740ce7b600002b1010d87dfd3</link>
+</links>
+<cf v="null"/>
+</cr>
--- _charm/src/e56ba90338b15805e882937880d547edc3eb47a740ce7b600002b1010d87dfd3.cr
+++ _charm/src/e56ba90338b15805e882937880d547edc3eb47a740ce7b600002b1010d87dfd3.cr
@@ -15,6 +15,7 @@
 <link name="child">42add6be255e360828f716d66fd70634b21d83023e8be2dd9ca2bac2ba4ecfe9</link>
 <link name="child">9d97d227d58ce8f6871140b6ee2bc2a1e7a5fddef2e5a41b6fb830a7e172a253</link>
 <link name="child">82cb5d3afb7d8c3d3d5dfedf29334fca1a7f328f7edba73f11c30890af08890a</link>
+<link name="child">c19610976effdbebf71c202fb721af73ae179d30d89b934a540bf212af98cb36</link>
 </links>
 <cf v="sdevaux 2024-12-21T16:33:58+01:00"/>
 </cr>
