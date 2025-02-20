7d0e2cd332457a11fe1d776f41749636db11ef5b
Sebastien Devaux <sebastien.devaux@eduvax.net>
2025-02-20 13:35:51 +0100

    #c196109 deleting smpdk module from simphonie branch

--- smpdk/test/DummyComponent.cpp
+++
@@ -1,6 +0,0 @@
-#include "DummyComponent.hpp"
-
-namespace test {
-DummyComponent::DummyComponent(Smp::String8 name, Smp::String8 descr, Smp::IObject* parent)
-    : Component(name, descr, parent) {}
-}  // namespace test
