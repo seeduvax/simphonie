83d3651527ca8fbc8e408b79ebf1c46edd74c4f4
Martin de gastines <martindegastines@gmail.com>
2025-03-12 15:15:30 +0000

    #8040ac4 issue management

---
+++ _charm/src/5ca5435e7fbaebc47805d66f9701408c1ed167dd73b0546aae8e0fc7955938d7.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="5ca5435e7fbaebc47805d66f9701408c1ed167dd73b0546aae8e0fc7955938d7" state="open">
+<title>fix constexpr debian11</title>
+<reporter>f003181</reporter>
+<creation>2025-03-12 15:15:30+00:00</creation>
+<description></description>
+<links>
+<link name="parent">8040ac4dc477eabb3c005d795aaf11bbcdd0a421b2c6767547b379ad3fb92530</link>
+</links>
+<cf v="null"/>
+</cr>
--- _charm/src/8040ac4dc477eabb3c005d795aaf11bbcdd0a421b2c6767547b379ad3fb92530.cr
+++ _charm/src/8040ac4dc477eabb3c005d795aaf11bbcdd0a421b2c6767547b379ad3fb92530.cr
@@ -8,6 +8,7 @@
 <link name="child">f812cea1d4f3c7281a6bbfd10e4009134ab8ac7a23e61ed0be230868d7ab73fd</link>
 <link name="child">8ac0232c36662d6afb46f7368327749f5536b22c3cef31e0c6de216df3c98b29</link>
 <link name="child">e847f7993d6600393e6e64b72903e305d2ee44cb6f361310ca095c0542432d5e</link>
+<link name="child">5ca5435e7fbaebc47805d66f9701408c1ed167dd73b0546aae8e0fc7955938d7</link>
 </links>
 <cf v="m096258 2025-03-10T15:47:37+01:00"/>
 </cr>
