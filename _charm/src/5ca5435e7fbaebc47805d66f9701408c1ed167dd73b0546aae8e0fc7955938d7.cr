8c7413ab34040475ff1562d758f264d7f783c4d0
Martin de gastines <martindegastines@gmail.com>
2025-03-12 15:15:57 +0000

    #5ca5435 fix constexpr debian11

--- _charm/src/5ca5435e7fbaebc47805d66f9701408c1ed167dd73b0546aae8e0fc7955938d7.cr
+++ _charm/src/5ca5435e7fbaebc47805d66f9701408c1ed167dd73b0546aae8e0fc7955938d7.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="5ca5435e7fbaebc47805d66f9701408c1ed167dd73b0546aae8e0fc7955938d7" state="open">
+<cr id="5ca5435e7fbaebc47805d66f9701408c1ed167dd73b0546aae8e0fc7955938d7" state="working">
 <title>fix constexpr debian11</title>
 <reporter>f003181</reporter>
 <creation>2025-03-12 15:15:30+00:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">8040ac4dc477eabb3c005d795aaf11bbcdd0a421b2c6767547b379ad3fb92530</link>
 </links>
-<cf v="null"/>
+<cf v="f003181 2025-03-12T15:15:57+00:00"/>
 </cr>
