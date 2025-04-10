9d191b423f10dec3dafa20f7c6ad45699a235094
Sebastien Devaux <sebastien.devaux@eduvax.net>
2025-04-10 09:01:41 +0200

    #310b0f7 issue management

--- _charm/src/310b0f732463a38bc95b36139817dc0e677037a3a5c1d109080c7c2bc923fb7e.cr
+++ _charm/src/310b0f732463a38bc95b36139817dc0e677037a3a5c1d109080c7c2bc923fb7e.cr
@@ -22,6 +22,7 @@
 <link name="child">d57b00e5c661f2fba57e2a0942e1bedce2a84e3faf3cb15ddf723937c6716318</link>
 <link name="child">0568ed4ff93f0adc99de46e401d160e898e03d58fc1f5f049eb7aa22a7af5859</link>
 <link name="child">db4859787e9f91130448611d4a97fef093d0bc755a29417a1c60e6118e819edb</link>
+<link name="child">fd305b920439c74c77487d59654c6ca91986437431f03ca625f38d038c5d2909</link>
 </links>
 <cf v="Sebastien Devaux 2025-04-10T08:44:59+02:00"/>
 </cr>
