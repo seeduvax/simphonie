35903475bdc4ff545ebc8032723c5e9ff68192ad
Sebastien Devaux <sebastien.devaux@eduvax.net>
2025-04-10 12:24:47 +0200

    #fd305b9 spec logger

--- _charm/src/fd305b920439c74c77487d59654c6ca91986437431f03ca625f38d038c5d2909.cr
+++ _charm/src/fd305b920439c74c77487d59654c6ca91986437431f03ca625f38d038c5d2909.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="fd305b920439c74c77487d59654c6ca91986437431f03ca625f38d038c5d2909" state="open">
+<cr id="fd305b920439c74c77487d59654c6ca91986437431f03ca625f38d038c5d2909" state="working">
 <title>More complete logger service</title>
 <reporter>sdevaux</reporter>
 <creation>2025-04-10 09:01:41+02:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">310b0f732463a38bc95b36139817dc0e677037a3a5c1d109080c7c2bc923fb7e</link>
 </links>
-<cf v="null"/>
+<cf v="Sebastien Devaux 2025-04-10T12:24:53+02:00"/>
 </cr>
