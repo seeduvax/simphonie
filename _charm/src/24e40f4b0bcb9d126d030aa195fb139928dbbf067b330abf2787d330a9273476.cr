43b4e47cfafe82b0a9958c1753bc4815bc7d0cf4
Sebastien Devaux <sebastien.devaux@eduvax.net>
2025-04-12 18:44:03 +0200

    #310b0f7 issue management

---
+++ _charm/src/24e40f4b0bcb9d126d030aa195fb139928dbbf067b330abf2787d330a9273476.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="24e40f4b0bcb9d126d030aa195fb139928dbbf067b330abf2787d330a9273476" state="open">
+<title>Storage reader and writer</title>
+<reporter>sdevaux</reporter>
+<creation>2025-04-12 18:44:03+02:00</creation>
+<description></description>
+<links>
+<link name="parent">310b0f732463a38bc95b36139817dc0e677037a3a5c1d109080c7c2bc923fb7e</link>
+</links>
+<cf v="null"/>
+</cr>
