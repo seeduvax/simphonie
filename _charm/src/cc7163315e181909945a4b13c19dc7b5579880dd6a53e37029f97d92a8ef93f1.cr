b3b18220671685c4961d6629f355b51325940e5e
Sebastien Devaux <sebastien.devaux@eduvax.net>
2025-04-28 13:49:30 +0200

    #cc71633 thinking about SMP extensions

--- _charm/src/cc7163315e181909945a4b13c19dc7b5579880dd6a53e37029f97d92a8ef93f1.cr
+++ _charm/src/cc7163315e181909945a4b13c19dc7b5579880dd6a53e37029f97d92a8ef93f1.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="cc7163315e181909945a4b13c19dc7b5579880dd6a53e37029f97d92a8ef93f1" state="open">
+<cr id="cc7163315e181909945a4b13c19dc7b5579880dd6a53e37029f97d92a8ef93f1" state="working">
 <title>SMP extension specification</title>
 <reporter>m026258</reporter>
 <creation>2025-04-28 13:49:26+02:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">310b0f732463a38bc95b36139817dc0e677037a3a5c1d109080c7c2bc923fb7e</link>
 </links>
-<cf v="null"/>
+<cf v="Sebastien Devaux 2025-04-28T13:49:45+02:00"/>
 </cr>
