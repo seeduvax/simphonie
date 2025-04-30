093057f0e05be246617b2d18a4f029fee36926d6
Titouan Abadie <titouan.abadie@tabadie.eu>
2025-04-30 14:03:55 +0200

    #fd305b9 Update logger's doc

--- _charm/src/fd305b920439c74c77487d59654c6ca91986437431f03ca625f38d038c5d2909.cr
+++ _charm/src/fd305b920439c74c77487d59654c6ca91986437431f03ca625f38d038c5d2909.cr
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">310b0f732463a38bc95b36139817dc0e677037a3a5c1d109080c7c2bc923fb7e</link>
 </links>
-<cf v="abadiet 2025-04-22T15:47:30+02:00"/>
+<cf v="Titouan Abadie 2025-04-30T14:04:12+02:00"/>
 </cr>
