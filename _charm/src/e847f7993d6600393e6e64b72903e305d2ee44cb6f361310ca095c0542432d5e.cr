cbdc35b62177a2262378956fa78e801636c47c8b
fred322 <astfred2@yahoo.fr>
2025-06-19 13:52:36 +0200

    #e847f79  [tag switch] tag simdeck-1.0.15

--- _charm/src/e847f7993d6600393e6e64b72903e305d2ee44cb6f361310ca095c0542432d5e.cr
+++ _charm/src/e847f7993d6600393e6e64b72903e305d2ee44cb6f361310ca095c0542432d5e.cr
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">8040ac4dc477eabb3c005d795aaf11bbcdd0a421b2c6767547b379ad3fb92530</link>
 </links>
-<cf v="fred322 2025-06-19T12:59:19+02:00"/>
+<cf v="fred322 2025-06-19T13:52:36+02:00"/>
 </cr>
