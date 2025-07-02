0e74547a0662e1e0d1c16a806e88edb50a10141e
Martin DE-GASTINES <martin.de-gastines@ariane.group>
2025-07-02 15:55:25 +0200

    #328dd36  [tag switch] simphonie-1.0.9

--- _charm/src/328dd36e61b898b2003f0590aece878c57739afc7f22ec5c4335452f4a0ef3e4.cr
+++ _charm/src/328dd36e61b898b2003f0590aece878c57739afc7f22ec5c4335452f4a0ef3e4.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="328dd36e61b898b2003f0590aece878c57739afc7f22ec5c4335452f4a0ef3e4" state="open">
+<cr id="328dd36e61b898b2003f0590aece878c57739afc7f22ec5c4335452f4a0ef3e4" state="working">
 <title>C++ string field support</title>
 <reporter>f003181</reporter>
 <creation>2025-07-02 15:49:36+02:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">310b0f732463a38bc95b36139817dc0e677037a3a5c1d109080c7c2bc923fb7e</link>
 </links>
-<cf v="null"/>
+<cf v="f003181 2025-07-02T15:55:25+02:00"/>
 </cr>
