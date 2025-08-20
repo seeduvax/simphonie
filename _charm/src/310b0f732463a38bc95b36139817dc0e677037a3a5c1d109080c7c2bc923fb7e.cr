51f8bdb254f2f213f26f38622a714011b8b61e74
Titouan Abadie <titouan.abadie@tabadie.eu>
2025-08-20 16:47:30 +0200

    #310b0f7 issue management

--- _charm/src/310b0f732463a38bc95b36139817dc0e677037a3a5c1d109080c7c2bc923fb7e.cr
+++ _charm/src/310b0f732463a38bc95b36139817dc0e677037a3a5c1d109080c7c2bc923fb7e.cr
@@ -31,6 +31,7 @@
 <link name="child">da35322963af39c245316d3d2dd294ed74d96976ba52833c705bfeb314b82ba6</link>
 <link name="child">328dd36e61b898b2003f0590aece878c57739afc7f22ec5c4335452f4a0ef3e4</link>
 <link name="child">c5f6371e9b80cece7e3725224eeeb20cb7a5a204546c7e3c2670dfb1ea47db06</link>
+<link name="child">46ed1efb214edc6e824ac0f79239f1e8013278681be7aebf41ad5928cdb477a9</link>
 </links>
 <cf v="Titouan Abadie 2025-07-31T11:22:21+02:00"/>
 </cr>
---
+++ _charm/src/46ed1efb214edc6e824ac0f79239f1e8013278681be7aebf41ad5928cdb477a9.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="46ed1efb214edc6e824ac0f79239f1e8013278681be7aebf41ad5928cdb477a9" state="open">
+<title>FMI Support</title>
+<reporter>f005921</reporter>
+<creation>2025-08-20 16:47:30+02:00</creation>
+<description></description>
+<links>
+<link name="parent">310b0f732463a38bc95b36139817dc0e677037a3a5c1d109080c7c2bc923fb7e</link>
+</links>
+<cf v="null"/>
+</cr>
