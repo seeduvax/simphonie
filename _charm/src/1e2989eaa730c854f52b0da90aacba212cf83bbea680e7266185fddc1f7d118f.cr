4df8e074b93c19c3c5e8cdababdc8f69a71de9fc
Sebastien Devaux <sebastien.devaux@eduvax.net>
2025-11-10 18:28:54 +0100

    #1e2989e wip definition file
    Moved some parts to simdeck definition file (see simdeck-1.0 branch)

--- _charm/src/1e2989eaa730c854f52b0da90aacba212cf83bbea680e7266185fddc1f7d118f.cr
+++ _charm/src/1e2989eaa730c854f52b0da90aacba212cf83bbea680e7266185fddc1f7d118f.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="1e2989eaa730c854f52b0da90aacba212cf83bbea680e7266185fddc1f7d118f" state="open">
+<cr id="1e2989eaa730c854f52b0da90aacba212cf83bbea680e7266185fddc1f7d118f" state="working">
 <title>Simphonie definition file</title>
 <reporter>sdevaux</reporter>
 <creation>2024-12-21 16:29:49+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">310b0f732463a38bc95b36139817dc0e677037a3a5c1d109080c7c2bc923fb7e</link>
 </links>
-<cf v="null"/>
+<cf v="Sebastien Devaux 2025-11-10T18:29:33+01:00"/>
 </cr>
