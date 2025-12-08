f8e3772564ea0ddb3fa621659885556dababc230
Sebastien Devaux <sebastien.devaux@eduvax.net>
2025-12-08 18:45:47 +0100

    #41abb10  [tag switch] pub scheduler fixes and improvements

--- _charm/src/41abb10a9349cd2bd0d5d0fc06414b4b5ee66e78e0c3fd7568f7af123751919f.cr
+++ _charm/src/41abb10a9349cd2bd0d5d0fc06414b4b5ee66e78e0c3fd7568f7af123751919f.cr
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">310b0f732463a38bc95b36139817dc0e677037a3a5c1d109080c7c2bc923fb7e</link>
 </links>
-<cf v="Sebastien Devaux 2025-12-08T18:45:07+01:00"/>
+<cf v="Sebastien Devaux 2025-12-08T18:45:47+01:00"/>
 </cr>
