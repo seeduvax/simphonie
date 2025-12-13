25371e6e395daa422362615c85340f4aa065a258
Sebastien Devaux <sebastien.devaux@eduvax.net>
2025-12-13 18:03:00 +0100

    #130381f removed interfaces no more needed with meta scheduler

--- _charm/src/130381fd962e920882bcb7c551ff9e399faf56b44cca7c55d35c5812116b97ac.cr
+++ _charm/src/130381fd962e920882bcb7c551ff9e399faf56b44cca7c55d35c5812116b97ac.cr
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">8040ac4dc477eabb3c005d795aaf11bbcdd0a421b2c6767547b379ad3fb92530</link>
 </links>
-<cf v="Sebastien Devaux 2025-11-23T22:21:05+01:00"/>
+<cf v="Sebastien Devaux 2025-12-13T18:03:37+01:00"/>
 </cr>
