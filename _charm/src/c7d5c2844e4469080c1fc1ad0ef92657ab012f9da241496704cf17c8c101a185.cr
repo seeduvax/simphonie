c27af25e9d2861361e1f83229af9af435f33fa83
Sebastien Devaux <sebastien.devaux@eduvax.net>
2026-02-02 18:19:14 +0100

    #c7d5c28  [tag switch] pub front

--- _charm/src/c7d5c2844e4469080c1fc1ad0ef92657ab012f9da241496704cf17c8c101a185.cr
+++ _charm/src/c7d5c2844e4469080c1fc1ad0ef92657ab012f9da241496704cf17c8c101a185.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="c7d5c2844e4469080c1fc1ad0ef92657ab012f9da241496704cf17c8c101a185" state="open">
+<cr id="c7d5c2844e4469080c1fc1ad0ef92657ab012f9da241496704cf17c8c101a185" state="working">
 <title>Simulator front end extending ISimulator API by delegation</title>
 <reporter>m026258</reporter>
 <creation>2026-02-02 18:18:26+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">8040ac4dc477eabb3c005d795aaf11bbcdd0a421b2c6767547b379ad3fb92530</link>
 </links>
-<cf v="null"/>
+<cf v="Sebastien Devaux 2026-02-02T18:19:14+01:00"/>
 </cr>
