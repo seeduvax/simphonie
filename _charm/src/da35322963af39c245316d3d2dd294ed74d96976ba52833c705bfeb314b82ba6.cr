22a83414f5ef678b6fe7270a0b58f81789f00eb2
Sebastien Devaux <sebastien.devaux@eduvax.net>
2026-02-24 13:15:39 +0100

    #da35322 tracking memory leaks

--- _charm/src/da35322963af39c245316d3d2dd294ed74d96976ba52833c705bfeb314b82ba6.cr
+++ _charm/src/da35322963af39c245316d3d2dd294ed74d96976ba52833c705bfeb314b82ba6.cr
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">310b0f732463a38bc95b36139817dc0e677037a3a5c1d109080c7c2bc923fb7e</link>
 </links>
-<cf v="Titouan Abadie 2025-06-17T10:42:21+02:00"/>
+<cf v="Sebastien Devaux 2026-02-24T13:16:02+01:00"/>
 </cr>
--- mt/test/TestSimSyncDataShare.cpp
+++ mt/test/TestSimSyncDataShare.cpp
@@ -25,77 +25,80 @@ ABS_TEST_SUITE_BEGIN(SimSyncDataShare)
 private:
     class FakeSimulator : public virtual Smp::ISimulator {
     public:
-        inline FakeSimulator() : _treg(new simphonie::kern::TypeRegistry("TypeRegistry", "", nullptr)) {}
-        inline Smp::Publication::ITypeRegistry* GetTypeRegistry() const override {
+        FakeSimulator() : _treg(new simphonie::kern::TypeRegistry("TypeRegistry", "", nullptr)) {}
+        virtual ~FakeSimulator() {
+            delete _treg;
+        }
+        Smp::Publication::ITypeRegistry* GetTypeRegistry() const override {
             return _treg;
         }
 
-        inline void Initialise() override {}
-        inline void Publish() override {}
-        inline void Configure() override {}
-        inline void Connect() override {}
-        inline void Run() override {}
-        inline void Hold(Smp::Bool immediate) override {}
-        inline void Store(Smp::String8 filename) override {}
-        inline void Restore(Smp::String8 filename) override {}
-        inline void Reconnect(Smp::IComponent* root) override {}
-        inline void Exit() override {}
-        inline void Abort() override {}
-        inline Smp::SimulatorStateKind GetState() const override {
+        void Initialise() override {}
+        void Publish() override {}
+        void Configure() override {}
+        void Connect() override {}
+        void Run() override {}
+        void Hold(Smp::Bool immediate) override {}
+        void Store(Smp::String8 filename) override {}
+        void Restore(Smp::String8 filename) override {}
+        void Reconnect(Smp::IComponent* root) override {}
+        void Exit() override {}
+        void Abort() override {}
+        Smp::SimulatorStateKind GetState() const override {
             return Smp::SimulatorStateKind::SSK_Standby;
         }
-        inline void AddInitEntryPoint(Smp::IEntryPoint* entryPoint) override {}
-        inline void AddModel(Smp::IModel* model) override {}
-        inline void AddService(Smp::IService* service) override {}
-        inline Smp::IService* GetService(Smp::String8 name) const override {
+        void AddInitEntryPoint(Smp::IEntryPoint* entryPoint) override {}
+        void AddModel(Smp::IModel* model) override {}
+        void AddService(Smp::IService* service) override {}
+        Smp::IService* GetService(Smp::String8 name) const override {
             return nullptr;
         }
-        inline Smp::Services::ILogger* GetLogger() const override {
+        Smp::Services::ILogger* GetLogger() const override {
             return nullptr;
         }
-        inline Smp::Services::ITimeKeeper* GetTimeKeeper() const override {
+        Smp::Services::ITimeKeeper* GetTimeKeeper() const override {
             return nullptr;
         }
-        inline Smp::Services::IScheduler* GetScheduler() const override {
+        Smp::Services::IScheduler* GetScheduler() const override {
             return nullptr;
         }
-        inline Smp::Services::IEventManager* GetEventManager() const override {
+        Smp::Services::IEventManager* GetEventManager() const override {
             return nullptr;
         }
-        inline Smp::Services::IResolver* GetResolver() const override {
+        Smp::Services::IResolver* GetResolver() const override {
             return nullptr;
         }
-        inline Smp::Services::ILinkRegistry* GetLinkRegistry() const override {
+        Smp::Services::ILinkRegistry* GetLinkRegistry() const override {
             return nullptr;
         }
-        inline void RegisterFactory(Smp::IFactory* componentFactory) override {}
-        inline Smp::IComponent* CreateInstance(Smp::Uuid uuid, Smp::String8 name, Smp::String8 description,
+        void RegisterFactory(Smp::IFactory* componentFactory) override {}
+        Smp::IComponent* CreateInstance(Smp::Uuid uuid, Smp::String8 name, Smp::String8 description,
                                                Smp::IComposite* parent) override {
             return nullptr;
         }
-        inline Smp::IFactory* GetFactory(Smp::Uuid uuid) const override {
+        Smp::IFactory* GetFactory(Smp::Uuid uuid) const override {
             return nullptr;
         }
-        inline const Smp::FactoryCollection* GetFactories() const override {
+        const Smp::FactoryCollection* GetFactories() const override {
             return nullptr;
         }
-        inline void LoadLibrary(Smp::String8 libraryPath, Smp::LibraryLoadingFlag flag) override {}
-        inline const Smp::ContainerCollection* GetContainers() const override {
+        void LoadLibrary(Smp::String8 libraryPath, Smp::LibraryLoadingFlag flag) override {}
+        const Smp::ContainerCollection* GetContainers() const override {
             return nullptr;
         }
-        inline Smp::IContainer* GetContainer(Smp::String8 name) const override {
+        Smp::IContainer* GetContainer(Smp::String8 name) const override {
             return nullptr;
         }
-        inline Smp::String8 GetName() const {
+        Smp::String8 GetName() const {
             return nullptr;
         }
-        inline Smp::String8 GetDescription() const override {
+        Smp::String8 GetDescription() const override {
             return nullptr;
         }
-        inline Smp::IObject* GetParent() const override {
+        Smp::IObject* GetParent() const override {
             return nullptr;
         }
-        inline Smp::IObject* GetChild(Smp::String8 name) const override {
+        Smp::IObject* GetChild(Smp::String8 name) const override {
             return nullptr;
         }
 
