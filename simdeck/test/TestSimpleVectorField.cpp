1eb0e3bf14597947c7f564357135fd95d92a7bb3
AdrienSourisseau <a.b@gmail.com>
2026-03-12 09:48:06 +0000

    #4fb0d80 forget to commit

--- simdeck/test/TestSimpleVectorField.cpp
+++ simdeck/test/TestSimpleVectorField.cpp
@@ -32,7 +32,7 @@ public:
 
     ABS_TEST_CASE_BEGIN(VectorWrapping) {
         std::vector<Smp::Int32> vint={1,2,4,8,16,32};
-        auto vf=SimpleVectorField::Create("vint","",&vint,&_vectorInt64Type,
+        auto vf=SimpleVectorField::Create("vint","",(void*)&vint,&_vectorInt64Type,
                 Smp::ViewKind::VK_All, &_vectorInt64Type,
                 false, true, false, nullptr);
         CPPUNIT_ASSERT_EQUAL(vint.size(), vf->GetSize());
